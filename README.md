89287a98abdf47017dfef593e80fa350da064a68
Gian Lorenzetto <gian.lorenzetto@readify.net>
2015-10-15 15:12:06 +0800

    Initial script file. Updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# azure
+# Azure Utils
+
+Azure specific scripts and utilities.
+
+## Scripts
+
+1. create-vm
+
+Interactive cmdlet to create a VM in Azure. Prompts for the account details and the type of VM.
---
+++ scripts/create-vm.ps1
@@ -0,0 +1,7 @@
+# initialise credentials and associated subscription within which
+# the VM will be created
+
+# NB: this is an interactive cmdlet and will open a prompt to enter
+# user name and password
+
+Add-AzureAccount
