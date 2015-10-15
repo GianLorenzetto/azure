8af69175185e20606acd7bc2b8edb45ca3a8f838
Gian Lorenzetto <gian.lorenzetto@readify.net>
2015-10-16 07:32:33 +0800

    Updated .gitignore to ignore local json configuration files. Modified the create VM script to read config from a json file.

--- scripts/create-vm.ps1
+++ scripts/create-vm.ps1
@@ -1,7 +1,36 @@
-# initialise credentials and associated subscription within which
-# the VM will be created
+function Has-Property($object, \[string\] $propertyName)
+{
+    $object.PSObject.Properties.Match($propertyName).Count > 0
+}
 
-# NB: this is an interactive cmdlet and will open a prompt to enter
-# user name and password
+# read VM details
 
-Add-AzureAccount
+$config = (Get-Content vm_details.json) -join "`n" | ConvertFrom-Json
+
+##
+## initialise credentials and associated subscription within which
+## the VM will be created
+##
+
+"Adding account '" + $config.accountName + "'"
+
+if ((Has-Property $config, 'accountUsername') -and `
+    (Has-Property $config, 'accountPassword'))
+{
+    "creating credentials"
+
+    $securePassword = ConvertTo-SecureString $config.password -AsPlainText -Force
+    $credentials = New-Object System.Management.Automation.PSCredential ($config.accountUsername, $securePassword)
+    #Add-AzureAccount -Credential $credentials
+}
+else
+{
+    # NB: this is an interactive cmdlet and will open a prompt to enter
+    # user name and password
+
+"going interactive"
+
+    #Add-AzureAccount
+}
+
+"Creating VM '" + $config.vmName + "'"
