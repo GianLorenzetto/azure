2edb16f3cd94a9478cc116e686a08e9057dfe42d
Gian Lorenzetto <gian.lorenzetto@readify.net>
2015-10-16 20:55:31 +0800

    First pass at completed script to create VM. Still need to handle missing parameters and multiple VM configs in the json file.

--- scripts/create-vm.ps1
+++ scripts/create-vm.ps1
@@ -11,22 +11,44 @@ $config = Get-ConfigFromJson vm_details.json
 ## the VM will be created
 ##
 
-if ((Has-Property $config 'accountUsername') -and `
-    (Has-Property $config 'accountPassword'))
+if (Has-Property $config 'account')
 {
-    "** Creating credentials for username '" + $config.accountUsername + "'"
+    Write-InfoMessage("Creating credentials for username '" + $config.account.username + "'")
 
-    $securePassword = ConvertTo-SecureString $config.accountPassword -AsPlainText -Force
-    $credentials = New-Object System.Management.Automation.PSCredential ($config.accountUsername, $securePassword)
+    $securePassword = ConvertTo-SecureString $config.account.password -AsPlainText -Force
+    $credentials = New-Object System.Management.Automation.PSCredential ($config.account.username, $securePassword)
     Add-AzureAccount -Credential $credentials
 }
 else
 {
-    "** Interactive account prompt required ..."
+    Write-InfoMessage("Interactive account prompt required ...")
 
     # NB: this is an interactive cmdlet and will open a prompt to enter
     # user name and password
-    Add-AzureAccount
+    #Add-AzureAccount
 }
 
-"Creating VM '" + $config.vmName + "'"
+# TODO From here this should be a loop over each 'vm' in the config
+
+if (Has-Property $config.vm 'subscriptionName' -and `
+    Has-Property $config.vm 'storageAccount')
+{
+    Write-InfoMessage("Setting storage account to: " + $config.vm.storageAccount)
+    Set-StorageAccount $config.vm.subscriptionName $config.vm.storageAccount
+}
+else 
+{
+    # TODO try and locate the default storage account? will also need the default subscription name
+}
+
+if (-Not (Has-Property $config.vm 'serviceName')
+{
+    # TODO try and locate the default service
+}
+
+Write-InfoMessage("Creating VM '" + $config.vm.name + "' ... (this can take serveral minutes)")
+$result = Create-NewVm($config)
+Write-InfoMessage("... done!")
+
+Write-InfoMessage("Result: ")
+$result
--- scripts/utils.ps1
+++ scripts/utils.ps1
@@ -1,3 +1,10 @@
+# Convenience method for writing a simple info message. Text
+# is written in a non-default colour.
+function Write-InfoMessage($msg)
+{
+    Write-Host $msg `n -foregroundcolor DarkCyan
+}
+
 # Read VM config from the given JSON file
 function Get-ConfigFromJson($pathToJson)
 {
@@ -9,4 +16,80 @@ function Get-ConfigFromJson($pathToJson)
 function Has-Property($object, [string] $propertyName)
 {
     $object.PSObject.Properties.Match($propertyName).Count
+}
+
+# Get all Azure VMs with a matching label. The label can contain
+# wild cards and is filtered on a 'like' condition.
+# NB To call this function, the session must have already authenticated
+#    with an Azure account
+# TODO CMDLET
+function List-VmDetails([string] $labelFilter)
+{
+    $vms = Get-AzureVMImage
+    $filtered = $vms | Where-Object { $_.Label -like $labelFilter }
+    $filtered | ForEach-Object { 
+        Write-Host $_.Label -foregroundcolor yellow; 
+        Write-Host "  Published: "$_.PublishedDate; 
+        Write-Host "  ImageName: "$_.ImageName 
+    }
+}
+
+# Get available role sizes. Role sizes are sorted on the instance
+# size and formatted as a table.
+# NB To call this function, the session must have already authenticated
+#    with an Azure account
+# TODO CMDLET
+function List-RoleSizes()
+{
+    Get-AzureRoleSize | Sort-Object InstanceSize | Format-Table
+}
+
+# List all availabe subscriptions, including the default subscription.
+function List-Subscriptions()
+{
+    Get-AzureSubscription | ForEach-Object {
+        $postfix = if ($_.IsDefault -eq 'True') { '* (Default)' } else { '' } 
+        Write-Host $_.SubscriptionName" "$postfix -foregroundcolor yellow
+    }
+}
+
+# List all available storage accounts, along with some basic details.
+function List-StorageAccounts()
+{
+    $storageAccounts = Get-AzureStorageAccount
+    $storageAccounts | ForEach-Object {
+        Write-Host $_.Label -foregroundcolor yellow;
+        Write-Host "  Desc:     "$_.StorageAccountDescription;
+        Write-Host "  Location: "$_.Location;
+        Write-Host "  Type:     "$_.AccountType
+    }
+}
+
+#List available services.
+function List-Services()
+{
+    Get-AzureService | ForEach-Object {
+        Write-Host $_.Label -foregroundcolor yellow
+        Write-Host "  Location: "$_.Location
+    }
+}
+
+# Set the storage account against a subscription.
+# NB To call this function, the session must have already authenticated
+#    with an Azure account 
+function Set-StorageAccount([string] $subscriptionName, [string] $storageAccount)
+{
+    Set-AzureSubscription -SubscriptionName $subscriptionName -CurrentStorageAccount $storageAccount
+}
+
+# Create a new virtual machine.
+# NB To call this function, the session must have already authenticated
+#    with an Azure account 
+function Create-NewVm($config)
+{
+    $secureAdminPassword = Convertto-Securestring -String $config.vm.adminPassword -AsPlainText -Force
+
+    New-AzureVMConfig -Name $config.vm.name -InstanceSize $config.vm.size -ImageName $config.vm.imageName `
+    | Add-AzureProvisioningConfig -Windows -Password $secureAdminPassword -AdminUsername $config.vm.adminUsername  `
+    | New-AzureVM -ServiceName $config.vm.serviceName -WaitForBoot
 }
